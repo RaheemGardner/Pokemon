bfa93b1fbe7a61c0740e00b9edfb73b80e09a004
RaheemGardner <rgardnerit@gmail.com>
2026-06-29 07:02:47 -0400

    Oaks Greeting

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -9,9 +9,38 @@ void functionName()
     std::cout << "Greetings " << player_Name << " looks like you have understood the main() function properly now!" <<std::endl;
     
 }
+void OaksGreeting() 
+{
+    std::cout<< "Welcome to the world of Pokémon! I am Professor Oak.\n"; 
+    std::cout << "You can choose one of the following Pokémon:\n"; 
+    std::cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
+    std::cout << "Which Pokémon would you like to choose? Enter the number: ";
+    int startingPokemonNumber;
+    std::cin >> startingPokemonNumber;
+    if (startingPokemonNumber ==1) 
+    {
+        std::cout << "You chose Bulbasaur! A wise choice.\n";
+    }
+    else if (startingPokemonNumber ==2) 
+    {
+        std::cout << "You chose Charmander! A fiery choice.\n";
+    }
+    else if (startingPokemonNumber ==3)
+    {
+        std::cout << "You chose Squirtle! A cool choice.\n";
+    }
+    else 
+    {
+        std::cout << "Invalid choice. Please try again.\n";
+        OaksGreeting();
+    }
+
+
+}
 
 
 int main() {
     functionName();
+    OaksGreeting();
    return 0; 
 }
